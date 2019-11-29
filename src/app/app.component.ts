ab0829a209041c094943af5f7eccefdbe763b8bb
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-29 12:30:38 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,18 @@ import { Component } from "@angular/core";
 @Component({
   selector: "simple-root",
   template: `
-    <simple-car-details [model]="carModel" [year]="carYear">
+    <simple-car-details 
+      [model]="carModel" 
+      [year]="carYear">
     </simple-car-details>
+    
     <button (click)="updateCar()">Aggiorna auto</button>
   `
 })
 export class AppComponent {
   carModel = "Ferrari 250 GTO";
   carYear = 1962;
-  
+
   updateCar() {
     this.carModel = "Ferrari 288 GTO";
     this.carYear = 1984;
--- src/app/car-details.component.ts
+++ src/app/car-details.component.ts
@@ -5,16 +5,17 @@ import { Component, Input, SimpleChanges, OnChanges } from "@angular/core";
   template: `
     <p>{{ model }} - {{ year }}</p>
     <pre>
-<code>
-{{ changes }}
-</code>
-</pre>
+      <code>
+      {{ changes }}
+      </code>
+    </pre>
   `
 })
 export class CarDetailsComponent implements OnChanges {
   @Input() model;
   @Input() year;
   changes: string;
+  
   ngOnChanges(changes: SimpleChanges): void {
     this.changes = JSON.stringify(changes, null, 2);
   }

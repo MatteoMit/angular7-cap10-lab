d99b1c8b8cb2946e0bb880bd2f331adbb6c77e24
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-11 07:07:22 +0200

    1 file modified

--- src/app/car-details.component.ts
+++ src/app/car-details.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input, SimpleChanges, OnChanges } from "@angular/core";
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
-  selector: "simple-car-details",
+  selector: 'simple-car-details',
   template: `
     <p>{{ car.model }} - {{ car.year }}</p>
     <pre>
-<code>
-{{ changes }}
-</code>
-</pre>
-  `
+      <code>
+        {{ changes }}
+      </code>
+    </pre>
+  `,
 })
 export class CarDetailsComponent implements OnChanges {
   @Input() car;
   changes: string;
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("ngOnchanges");
+    console.log('ngOnchanges');
     this.changes = JSON.stringify(changes, null, 2);
   }
 }
